cbb23a8f2034b4a16cd49a92c4e10721c1cd3bc5
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-05-31 15:34:43 -0400

    adding updates to manylinux builds
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- xacc_cronjobs/manylinux/ibm/build_ibm_manylinux_cron.sh
+++ xacc_cronjobs/manylinux/ibm/build_ibm_manylinux_cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --network=host -t xacc/build_ibm_wheels . --no-cache
-tmpid=$(docker run -d xacc/build_ibm_wheels bash -c "mkdir extract; cd extract ; mv /xacc-ibm/dist/*.whl /extract ") && sleep 5 && docker cp $tmpid:/extract . && docker rm -v $tmpid
+tmpid=$(docker run -d xacc/build_ibm_wheels bash -c "mkdir extract; cd extract ; mv /xacc-ibm/wheelhouse/*.whl /extract ") && sleep 5 && docker cp $tmpid:/extract . && docker rm -v $tmpid
 ls extract
--- xacc_cronjobs/manylinux/tnqvm/build_tnqvm_manylinux_cron.sh
+++ xacc_cronjobs/manylinux/tnqvm/build_tnqvm_manylinux_cron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build --network=host -t xacc/build_tnqvm_wheels . --no-cache
-tmpid=$(docker run -d xacc/build_tnqvm_wheels bash -c "mkdir extract; cd extract ; mv /tnqvm/dist/*.whl /extract ") && sleep 5 && docker cp $tmpid:/extract . && docker rm -v $tmpid
+tmpid=$(docker run -d xacc/build_tnqvm_wheels bash -c "mkdir extract; cd extract ; mv /tnqvm/wheelhouse/*.whl /extract ") && sleep 5 && docker cp $tmpid:/extract . && docker rm -v $tmpid
 ls extract
