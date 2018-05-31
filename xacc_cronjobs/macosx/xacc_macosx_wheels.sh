40ba8d190277a5dc146e722d4728ca657d0afb41
Alex McCaskey <mccaskeyaj@ornl.gov>
2018-05-31 15:35:14 -0400

    updates to macosx wheel builds
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- xacc_cronjobs/macosx/xacc_macosx_wheels.sh
+++ xacc_cronjobs/macosx/xacc_macosx_wheels.sh
@@ -12,31 +12,17 @@ mkdir -p $HOME/wheelhouse/vqe
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-function updateCprPath {
-	install_name_tool -change $1/tmp_build/lib/libcpr.dylib @rpath/libcpr.dylib $2
-}
-
-function updateBoostLibs {
-        install_name_tool -change libboost_system.dylib @rpath/libboost_system.dylib $1 
-        install_name_tool -change libboost_unit_test_framework.dylib @rpath/libboost_unit_test_framework.dylib $1 
-        install_name_tool -change libboost_filesystem.dylib @rpath/libboost_filesystem.dylib $1 
-        install_name_tool -change libboost_program_options.dylib @rpath/libboost_program_options.dylib $1 
-        install_name_tool -change libboost_regex.dylib @rpath/libboost_regex.dylib $1 
-        install_name_tool -change libboost_chrono.dylib @rpath/libboost_chrono.dylib $1 
-        install_name_tool -change libboost_graph.dylib @rpath/libboost_graph.dylib $1 
-}
-
 function clone {
 	git clone https://github.com/ornl-qci/$1
 }
 
-#git clone --recursive https://github.com/eclipse/xacc
-#clone xacc-rigetti
+git clone --recursive https://github.com/eclipse/xacc
+clone xacc-rigetti
 clone xacc-ibm
 clone xacc-vqe
 clone tnqvm 
 
-for version in 2.7.14 3.3.7 3.4.7 3.5.4 3.6.4
+for version in 2.7.14 3.3.7 3.4.7 3.5.4 3.6.4 
 do
 	pyenv virtualenv $version xacc-$version
 	pyenv activate xacc-$version
@@ -53,21 +39,6 @@ do
 	python setup.py build -t tmp_build --executable="/usr/bin/env python"
 	export buildPath=build/lib.$libPath-$ver
 	echo "./xacc" >> build/lib.$libPath-$ver/xacc.pth
-
-	install_name_tool -change libcpr.dylib @rpath/libcpr.dylib $buildPath/xacc/lib/libxacc.dylib
-	install_name_tool -add_rpath "@loader_path" $buildPath/xacc/lib/libboost_regex.dylib
-	install_name_tool -add_rpath "@loader_path" $buildPath/xacc/lib/libboost_chrono.dylib
-	install_name_tool -add_rpath "@loader_path" $buildPath/xacc/lib/libboost_filesystem.dylib
-	install_name_tool -add_rpath "@loader_path" $buildPath/xacc/lib/libboost_graph.dylib
-	install_name_tool -change libboost_system.dylib @rpath/libboost_system.dylib $buildPath/xacc/lib/libboost_filesystem.dylib
-	install_name_tool -change libboost_system.dylib @rpath/libboost_system.dylib $buildPath/xacc/lib/libboost_chrono.dylib
-	install_name_tool -change libboost_regex.dylib @rpath/libboost_regex.dylib $buildPath/xacc/lib/libboost_graph.dylib
-
-	updateCprPath $PWD $buildPath/xacc/pyxacc.so
-	updateBoostLibs $buildPath/xacc/lib/libxacc-quantum-gate.dylib
-	updateBoostLibs $buildPath/xacc/lib/libxacc-quantum-aqc.dylib
-	updateBoostLibs $buildPath/xacc/pyxacc.so
-
 	python setup.py bdist_wheel --skip-build
 	mv dist/*.whl $HOME/wheelhouse/xacc
 	
@@ -83,22 +54,13 @@ do
 	# ---------------- RIGETTI BUILD -------------------#
 	cd ../xacc-rigetti
         PYTHONPATH=../xacc/$buildPath/xacc python setup.py build -t tmp_build --executable="/usr/bin/env python"
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libxacc-rigetti-quilcompiler.dylib 
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libxacc-rigetti-accelerator.dylib 
-	updateBoostLibs $buildPath/xacc/plugins/libxacc-rigetti-quilcompiler.dylib
-	updateBoostLibs $buildPath/xacc/plugins/libxacc-rigetti-accelerator.dylib
 
 	python setup.py bdist_wheel --skip-build
 	mv dist/*.whl $HOME/wheelhouse/rigetti
 
 	# ---------------- IBM BUILD -------------------#
 	cd ../xacc-ibm
-
         PYTHONPATH=../xacc/$buildPath/xacc python setup.py build -t tmp_build --executable="/usr/bin/env python"
-
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libxacc-ibm-accelerator.dylib 
-	updateBoostLibs $buildPath/xacc/plugins/libxacc-ibm-accelerator.dylib
-
 	python setup.py bdist_wheel --skip-build
 	mv dist/*.whl $HOME/wheelhouse/ibm
 
@@ -106,12 +68,6 @@ do
 	cd ../tnqvm
 
         PYTHONPATH=../xacc/$buildPath/xacc python setup.py build -t tmp_build --executable="/usr/bin/env python"
-
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libtnqvm.dylib 
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libtnqvm-itensor.dylib 
-	updateBoostLibs $buildPath/xacc/plugins/libtnqvm.dylib
-	updateBoostLibs $buildPath/xacc/plugins/libtnqvm-itensor.dylib
-
 	python setup.py bdist_wheel --skip-build
 	mv dist/*.whl $HOME/wheelhouse/tnqvm
 
@@ -119,28 +75,12 @@ do
 	cd ../xacc-vqe
 
         PYTHONPATH=../xacc/$buildPath/xacc python setup.py build -t tmp_build --executable="/usr/bin/env python"
-
-	updateCprPath $xaccdir $buildPath/xacc/pyxaccvqe.so 
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libxacc-vqe-fermion-compiler.dylib 
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libxacc-vqe-fermion-compiler.dylib 
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libxacc-vqe-ir.dylib 
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libxacc-vqe-tasks.dylib 
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libxacc-vqe-irtransformations.dylib 
-	updateCprPath $xaccdir $buildPath/xacc/plugins/libxacc-vqe-no-mpi.dylib 
-
-	updateBoostLibs $buildPath/xacc/pyxaccvqe.so
-	updateBoostLibs $buildPath/xacc/plugins/libxacc-vqe-fermion-compiler.dylib
-	updateBoostLibs $buildPath/xacc/plugins/libxacc-vqe-ir.dylib
-	updateBoostLibs $buildPath/xacc/plugins/libxacc-vqe-tasks.dylib
-	updateBoostLibs $buildPath/xacc/plugins/libxacc-vqe-irtransformations.dylib
-	updateBoostLibs $buildPath/xacc/plugins/libxacc-vqe-no-mpi.dylib
-
 	python setup.py bdist_wheel --skip-build
 	mv dist/*.whl $HOME/wheelhouse/vqe
 
 	cd ..
 	python -m pip uninstall -y xacc
-
 	source deactivate
+	pyenv uninstall -f xacc-$version
 done
 
